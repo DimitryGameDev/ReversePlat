33fca802a47db56fc903371182e0a65051820f12
webresident <89592473+webresident@users.noreply.github.com>
2025-06-25 22:53:51 +0300

    Finished Enemy Simple Attack Behaviour

--- Assets/Scripts/Enemies/Projectiles/ChasingProjectile.cs
+++ Assets/Scripts/Enemies/Projectiles/ChasingProjectile.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class ChasingProjectile : Projectile, IProjectable
 {
     private Transform _chasingTarget;
+    private float _chasingHeight;
 
     private void Update()
     {
         
+        var direction = (_chasingTarget.position - _originTransform.position + new Vector3(0f, _chasingHeight / 2, 0)).normalized;
+        transform.Translate(direction * Time.deltaTime * _speed);
     }
     public void SetTargetTo(Transform target)
     {
+        _chasingHeight = target.GetComponent<Collider2D>().bounds.size.y;
         _chasingTarget = target;
     }
 }
--- Assets/Scripts/Enemies/RangeEnemy.cs
+++ Assets/Scripts/Enemies/RangeEnemy.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
 
+public enum ProjectileType
+{
+    SimpleProjectile,
+    ChasingProjectile
+}
+
+[RequireComponent(typeof(RangeWeapon))]
 public class RangeEnemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject _simpleProjectile;
+    [SerializeField] private GameObject _chasingProjectile;
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private float _detectionDistance = 10.0f;
+    [SerializeField] private ProjectileType _projectileType = ProjectileType.SimpleProjectile;
+    private RangeWeapon _weapon;
+    private Transform _detectedPlayer;
+    private Transform _originTransform;
+    private bool _isNextProjectile = true;
+    
+    private void Start()
     {
+        _weapon = GetComponent<RangeWeapon>();
+        switch (_projectileType)
+        {
+            case ProjectileType.SimpleProjectile:
+                _weapon.SetProjectileTo(_simpleProjectile);
+                break;
+            case ProjectileType.ChasingProjectile:
+                _weapon.SetProjectileTo(_chasingProjectile);
+                break;
+        }
         
+
+        _originTransform = transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _detectedPlayer = DetectPlayer();
+        if (_detectedPlayer != null)
+        {
+            AttackPlayer();
+        }
     }
+    private Transform DetectPlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(_originTransform.position, Vector2.right, _detectionDistance, _layerMask);
+        if (hit.collider != null)
+        {
+            return hit.collider.transform;
+        }
+
+        return null;
+    }
+
+    private void AttackPlayer()
+    {
+        if (_weapon.IsReloaded)
+        {
+            _weapon.LaunchProjectileTo(_detectedPlayer);
+
+        }
+    }
+
 }
