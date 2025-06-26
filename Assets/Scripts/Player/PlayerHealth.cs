b0d146a144eb897a020a9ea89c2c04cb3a9d83e0
webresident <89592473+webresident@users.noreply.github.com>
2025-06-27 00:17:29 +0300

    Added damage effect on Player + Spike variations

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,13 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerHealth : MonoBehaviour, IPlayerDamageable
 {
     [SerializeField] private int _maxHealth = 100;
+    [SerializeField] private float _colorChangeDuration = 0.2f;
+    [SerializeField] private Color _damageColor = Color.red;
     private int _health;
+    private SpriteRenderer _spriteRenderer;
+    private Color _originalColor;
+    
 
     private void Start()
     {
         _health = _maxHealth;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _originalColor = _spriteRenderer.color;
     }
     public void TakeDamage(int damage)
     {
@@ -19,6 +28,14 @@ public class PlayerHealth : MonoBehaviour, IPlayerDamageable
         {
             _health = 0;
         }
+        StartCoroutine(FlashOnTakenDamage());
         Debug.Log("Player HP is " + _health);
+    }
+
+    private IEnumerator FlashOnTakenDamage()
+    {
+        _spriteRenderer.color = _damageColor;
+        yield return new WaitForSeconds(_colorChangeDuration);
+        _spriteRenderer.color = _originalColor;
     }
 }
